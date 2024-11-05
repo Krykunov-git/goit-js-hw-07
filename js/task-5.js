7ae898cb4bdb5b36c1f61248bda83e069b6ef7fa
Ivan <hyundai27nn@gmail.com>
2024-11-05 22:02:21 +0100

    task 5++

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changecolorbutton = document.querySelector(".change-color");
+const colorspan = document.querySelector(".color");
+
+changecolorbutton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = `rgb(${parseInt(
+    newColor.slice(1, 3),
+    16
+  )}, ${parseInt(newColor.slice(3, 5), 16)}, ${parseInt(
+    newColor.slice(5, 7),
+    16
+  )}`;
+  сolorSpan.textContent = newColor;
+});
